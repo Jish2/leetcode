5e320c8ed36a482f51d2d635422b8095febea4ed
Joshua Goon <jgoon@tesla.com>
2025-04-24 15:14:28 -0700

    update daily

--- problems/2799.count-complete-subarrays-in-an-array.py
+++ problems/2799.count-complete-subarrays-in-an-array.py
@@ -1,3 +1,20 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        pass
+        c = defaultdict(int)
+        k = len(Counter(nums))
+        n = len(nums)
+
+        l = 0
+        ans = 0
+
+        for r in range(l, n):
+            c[nums[r]] += 1
+
+            while l < n and len(c) == k:
+                c[nums[l]] -= 1
+                if not c[nums[l]]: del c[nums[l]]
+                l += 1
+
+            ans += l
+
+        return ans
