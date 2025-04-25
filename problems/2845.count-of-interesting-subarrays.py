aa6c58c37c4f7e4166ecf55654ef073161066fb3
Joshua Goon <jgoon@tesla.com>
2025-04-25 09:18:41 -0700

    update solution;

--- problems/2845.count-of-interesting-subarrays.py
+++ problems/2845.count-of-interesting-subarrays.py
@@ -1,17 +1,15 @@
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
         n = len(nums)
-        p = [0] * n
         c = 0
-        for i, num in enumerate(nums):
-            if num % modulo == k: c += 1
-            p[i] = c
-
         ans = 0
+        hm = defaultdict(int)
+        hm[0] += 1
 
-        for r in range(n):
-            if p[r] % modulo == k:
-                ans += ceil(p[r] / modulo)
+        for num in nums:
+            if num % modulo == k: c += 1
+            ans += hm[(c - k + modulo) % modulo]
+            hm[c % modulo] += 1
 
         return ans
 
@@ -19,3 +17,4 @@ class Solution:
         # 01123
         #  ^^
         # ^   ^
+
